874a5d945c0e919ba4104bfca001877a34c624c7
anuchander <54872530+anuchander@users.noreply.github.com>
2022-11-04 21:33:35 -0700

    added TestClass2 in branch_01

---
+++ src/test/java/com/JenkinsProject/testcases/TestClass2.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.JenkinsProject.testcases;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author anu
+ *
+ */
+public class TestClass2 {
+	
+	@Test
+	public void method1() {
+		System.out.println("I am in method1 of TestClass2");
+	}
+
+}
