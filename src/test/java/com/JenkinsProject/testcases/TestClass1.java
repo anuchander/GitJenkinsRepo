66c0d7bb5cfa82c12eb071954745996cd4509ec2
anuchander <54872530+anuchander@users.noreply.github.com>
2022-11-06 21:50:38 -0800

    Resolved merge conflits, by incorporating logs and method5

--- src/test/java/com/JenkinsProject/testcases/TestClass1.java
+++ src/test/java/com/JenkinsProject/testcases/TestClass1.java
@@ -3,6 +3,8 @@
  */
 package com.JenkinsProject.testcases;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
 import org.testng.annotations.Test;
 import org.testng.annotations.Test;
 
@@ -14,19 +16,35 @@ import com.JenkinsProject.base.BaseClass;
  */
 public class TestClass1 extends BaseClass{
 	
+	Logger log = (Logger) LogManager.getLogger(TestClass1.class.getName());
+	//private static org.apache.logging.log4j.Logger demologger = LogManager.getLogger(TestClass1.class.getName());
+	
 	@Test
 	public void method1() {
+		log.info("This is before method1 in TestClass1");
 		System.out.println("This is message from method1");
+		log.info("This is after method1 in TestClass1");
 	}
 	
 	@Test
 	public void method2() {
+		log.info("This is before method2 in TestClass1");
 		System.out.println("This is message from method2");
+		log.info("This is after method2 in TestClass1");
 	}
 	
 	@Test
 	public void method3() {
+		log.info("This is before method3 in TestClass1");
 		System.out.println("This is message from method3");
+		log.info("This is after method3 in TestClass1");
+	}
+	
+	@Test
+	public void method5() {
+		log.info("This before nethod5 in TestClass1");
+		System.out.println("This is method5 for testing rebase");
+		log.info("This is after method5 in TestClass1");
 	}
 
 }
