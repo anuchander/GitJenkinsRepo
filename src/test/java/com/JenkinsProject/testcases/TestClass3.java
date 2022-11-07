66ecce80c483d3a5def22d782b38d2aa36566aa9
anuchander <54872530+anuchander@users.noreply.github.com>
2022-11-07 11:08:51 -0800

    Added method3 in TestClass3

--- src/test/java/com/JenkinsProject/testcases/TestClass3.java
+++ src/test/java/com/JenkinsProject/testcases/TestClass3.java
@@ -20,5 +20,10 @@ public class TestClass3 {
 	public void method2(){
 		System.out.println("This is method2 in TestClass3");
 	}
+	
+	@Test
+	public void method3() {
+		System.out.println("This is method3 in TestClass3");
+	}
 
 }
