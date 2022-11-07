4783a97ee36b8ea1f299b22a805f189b0feb2f2d
anuchander <54872530+anuchander@users.noreply.github.com>
2022-11-07 11:33:22 -0800

    added method6 in TestClass3 in github

--- src/test/java/com/JenkinsProject/testcases/TestClass3.java
+++ src/test/java/com/JenkinsProject/testcases/TestClass3.java
@@ -29,5 +29,10 @@ public class TestClass3 {
 		System.out.println("This is method4 in TestClass3 added in Github");
 
 	}
+	
+	@Test
+	public void method6(){
+		System.out.println("Added method6 in TestClass3 in Github");
+	}
 
 }
