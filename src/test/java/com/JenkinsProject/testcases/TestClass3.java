eb4218837495d6be92d8e80bbc45aba2d145fca3
anuchander <54872530+anuchander@users.noreply.github.com>
2022-11-07 11:00:47 -0800

    added third TestClass3

---
+++ src/test/java/com/JenkinsProject/testcases/TestClass3.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.JenkinsProject.testcases;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author anu
+ *
+ */
+public class TestClass3 {
+	
+	@Test
+	public void method1() {
+		System.out.println("This is method1 in TestClass3");
+	}
+
+}
