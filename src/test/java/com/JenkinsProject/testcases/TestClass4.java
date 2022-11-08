54b572cd402ed2d5a7c882b8a11a6f1be781b1bc
anuchander <54872530+anuchander@users.noreply.github.com>
2022-11-08 10:30:41 -0800

    added TestClass4 and method1

--- src/test/java/com/JenkinsProject/testcases/TestClass4.java
+++ src/test/java/com/JenkinsProject/testcases/TestClass4.java
@@ -3,10 +3,16 @@
  */
 package com.JenkinsProject.testcases;
 
+import org.testng.annotations.Test;
+
 /**
  * @author anu
  *
  */
 public class TestClass4 {
 
+	@Test
+	public void method1() {
+		System.out.println("This is method1 in TestClass4");
+	}
 }
