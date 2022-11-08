2947a5a2854e46ed2e10cb7839b8790b7b23cff3
anuchander <54872530+anuchander@users.noreply.github.com>
2022-11-08 10:47:39 -0800

    Create methodCreatedAtGitHub in TestClass4

--- src/test/java/com/JenkinsProject/testcases/TestClass4.java
+++ src/test/java/com/JenkinsProject/testcases/TestClass4.java
@@ -15,4 +15,9 @@ public class TestClass4 {
 	public void method1() {
 		System.out.println("This is method1 in TestClass4");
 	}
+	
+	@Test
+	public void methodCreatedAtGitHub(){
+		System.out.println("This method created in GitHub");
+	}
 }
