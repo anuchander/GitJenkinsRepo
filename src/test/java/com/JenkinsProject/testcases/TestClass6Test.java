dd55fa5cf433fe491007fa21ef6e6ec994ba5b68
anuchander <54872530+anuchander@users.noreply.github.com>
2022-12-19 20:38:10 -0800

    Added another line print statement to TestClass6Test.java

--- src/test/java/com/JenkinsProject/testcases/TestClass6Test.java
+++ src/test/java/com/JenkinsProject/testcases/TestClass6Test.java
@@ -3,6 +3,7 @@ package com.JenkinsProject.testcases;
 public class TestClass6Test{
 	public static void main (String [] args) {
 		System.out.println("Test test class 6");
+		System.out.println("Add another line");
 		
 	}
 }
