3968eddb895a219e738a051c1aa183ea31e24f65
anuchander <54872530+anuchander@users.noreply.github.com>
2022-12-27 19:17:34 -0800

    Added RebaseBranch1TestClass1 in RebaseBranch1 EOLVALIDATIONOVERIDE

---
+++ src/test/java/com/JenkinsProject/testcases/RebaseBranch1TestClass1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.JenkinsProject.testcases;
+
+/**
+ * @author anu
+ *
+ */
+public class RebaseBranch1TestClass1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
