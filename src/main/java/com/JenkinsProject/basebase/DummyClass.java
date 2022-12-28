393f1fcd30e7af345900925f5f13f56998d37c49
anuchander <54872530+anuchander@users.noreply.github.com>
2022-12-28 10:41:38 -0800

    Added print stmt in dummyclass in master branch

--- src/main/java/com/JenkinsProject/basebase/DummyClass.java
+++ src/main/java/com/JenkinsProject/basebase/DummyClass.java
@@ -13,7 +13,7 @@ public class DummyClass {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("This is master branch print stmt");
 
 	}
 
