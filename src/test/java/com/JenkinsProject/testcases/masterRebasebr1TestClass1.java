c61c8ef3abb6d40e0c29b9aae21fd0255015ba1f
anuchander <54872530+anuchander@users.noreply.github.com>
2022-12-28 10:18:41 -0800

    Created masterRebasebr1TestClass1 in master branch EOLVALIDATIONOVERIDE

---
+++ src/test/java/com/JenkinsProject/testcases/masterRebasebr1TestClass1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.JenkinsProject.testcases;
+
+/**
+ * @author anu
+ *
+ */
+public class masterRebasebr1TestClass1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("todo");
+
+	}
+
+}
