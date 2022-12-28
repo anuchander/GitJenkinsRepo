5595bfbfc5cabf226beefc10948aeeb3acb3deb3
anuchander <54872530+anuchander@users.noreply.github.com>
2022-12-27 19:29:57 -0800

    Added MasterTestClass.java in Master branch EOLVALIDATIONOVERIDE

---
+++ src/test/java/com/JenkinsProject/testcases/MasterTestClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.JenkinsProject.testcases;
+
+/**
+ * @author anu
+ *
+ */
+public class MasterTestClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
