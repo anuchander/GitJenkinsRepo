cd1971195c8730ed2a329b8bf41be3c5516e3961
anuchander <54872530+anuchander@users.noreply.github.com>
2022-12-28 10:36:44 -0800

    Added print statement in DummyClass in rebasebr1 branch

--- src/test/java/com/JenkinsProject/testcases/RebaseBranch1TestClass1.java
+++ src/test/java/com/JenkinsProject/testcases/RebaseBranch1TestClass1.java
@@ -13,7 +13,7 @@ public class RebaseBranch1TestClass1 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("RebaseBranch1TestClass1");
 
 	}
 
