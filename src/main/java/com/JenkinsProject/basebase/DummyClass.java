26c336644f5ada51e5a82558e7e73c256fc71df0
anuchander <54872530+anuchander@users.noreply.github.com>
2022-12-28 10:24:12 -0800

    Added basebase package and DummyClass in basebase package in master branch EOLVALIDATIONOVERIDE

---
+++ src/main/java/com/JenkinsProject/basebase/DummyClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.JenkinsProject.basebase;
+
+/**
+ * @author anu
+ *
+ */
+public class DummyClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
