b5b023a9de9c11fbc6eba3c3fc0ec92811ba3abf
anuchander <54872530+anuchander@users.noreply.github.com>
2023-01-20 22:55:11 -0800

    Branchtwo (#12)
    * Commited branch1Class.java in branch2 from stash
    
    * added class r1.java
    
    * Added branch1Class2.java from stash in branchtwo

---
+++ src/test/java/com/JenkinsProject/testcases/branch1Class2.java
@@ -0,0 +1,10 @@
+package com.JenkinsProject.testcases;
+
+public class branch1Class2 {
+
+	public static void main(String[] args) {
+		System.out.println("branch1 class2");
+
+	}
+
+}
---
+++ src/test/java/com/JenkinsProject/testcases/r1.java
@@ -0,0 +1,10 @@
+package com.JenkinsProject.testcases;
+
+public class r1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
