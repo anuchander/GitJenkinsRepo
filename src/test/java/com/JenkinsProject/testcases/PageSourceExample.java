c73ce00abf67d6e2015cc5bf5c359af2e3e72e67
anuchander <54872530+anuchander@users.noreply.github.com>
2023-02-17 16:36:31 -0800

    Add code to PageSource file

--- src/test/java/com/JenkinsProject/testcases/PageSourceExample.java
+++ src/test/java/com/JenkinsProject/testcases/PageSourceExample.java
@@ -1,10 +1,22 @@
 package com.JenkinsProject.testcases;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class PageSourceExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		WebDriver driver;
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		driver.get("https://www.business.linkedin.com");
+		String s = "Contact Linkedin";
+		Assert.assertTrue(driver.getPageSource().contains(s), "Contact Linkedin not present");
+		driver.close();
+		
 
 	}
 
