0d7f6504c9460fee2f4da2aaa256faa5d33feb1e
anuchander <54872530+anuchander@users.noreply.github.com>
2023-12-11 10:41:33 -0800

    Nb1 (#19)
    * added new class nclass1
    
    * renamed class to nclass

---
+++ src/test/java/com/JenkinsProject/testcases/nclass.java
@@ -0,0 +1,11 @@
+package com.JenkinsProject.testcases;
+
+public class nclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//test to rename file 
+
+	}
+
+}
