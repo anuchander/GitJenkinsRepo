cfe811d0486ba2a1a4f77fc098326dbf4b406230
anuchander <54872530+anuchander@users.noreply.github.com>
2024-02-10 22:10:58 -0800

    Create sample.js file

---
+++ src/test/java/com/JenkinsProject/testcases/sample.js
@@ -0,0 +1,12 @@
+class sample {
+
+    const firstname;
+    const lastname;
+    function myfunction(a,b){
+         firstname =a;
+         lastname = b;
+        }
+
+        console.log (firstname, lastname)
+
+}
